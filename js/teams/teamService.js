b8b26a53e6e6c12efdb9a5bfddcb11282a3ae611
Brian Kemper <bkemper@gmail.com>
2016-02-22 13:31:50 -0700

    WIP

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,7 +1,38 @@
-var app = angular.module('nbaRoutes');
+angular.module('nbaRoutes').service('teamService', function ($http) {
 
-app.service('teamService', function ($http, $q) {
+  this.addNewGame = function(gameObj) {
+    var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+      if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+          gameObj.won = true;
+      } else {
+        gameObj.won = false;
+      }
+      return  $http({
+        method: "POST",
+        url: url,
+        data: gameObj,
+        }).then(function(response) {
+          return response;
+      });
+  };
 
-    // service code
+  this.getTeamData = function(team){
+    var url = 'https://api.parse.com/1/classes/' + team;
+      return  $http({
+        method: "GET",
+        url: url,
+        data: gameObj,
+        }).then(function(response) {
+          var results = response.data.results;
+          var wins = 0;
+          var losses = 0;
+          results.forEach(function(obj){
+                  if (obj.won) {won ++;} else {losses ++;}
+          });
+          results.win = wins;
+          results.losses = losses;
+          return results;
+      });
+  };
 
 });
